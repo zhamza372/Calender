db6de48914507c816fd46630229b3adbc7098027
zhamza372 <hamza.zahid@7vals.com>
2020-08-24 10:06:52 +0500

    commit

--- assignment.rb
+++ assignment.rb
@@ -1,235 +1,261 @@
 require 'date'
 
 class Event
-	def initialize(title,description,date)
-		@title=title
-		@description=description
-		@date=date
-	end
-	def to_s
-		puts "Title="+@title+", " +"Desciption="+@description+", " +"Date="+@date.to_s
-	end
-	attr_accessor :title, :description,:date
-end
 
-class Menu
-	def displayMenu
-		puts "Press 1 to add event"
-		puts "Press 2 to update event"
-		puts "Press 3 to delete event"
-		puts "Press 4 to print the month view"
-		puts "Press 5 to print details of events in specific date"
-		puts "Press 6 to print the details of all events of specific month"
-		puts "Press 7 to exit"
-	end
-end
+  def initialize(title, description, date)
+    @title=title
+    @description=description
+    @date=date
+  end
 
-def addEvent(hash)
-	puts "Add an event"
-	begin
-		puts "Enter date in (yyyy-mm-dd) format"
-		date=Date.parse(gets.chomp)
-		puts "Enter Title of your event"
-		title=gets.chomp
-		puts "Enter Description of your event"
-		description=gets.chomp
-	rescue
-		puts "Please enter valid date"
-		retry
-	end
-	event=Event.new(title,description,date)
-	exist=0
+  def to_s
+    puts "Title="+@title+", " +"Desciption="+@description+", " +"Date="+ @date.to_s
+  end
 
-	if not hash.key?(date)
-		hash[date]=[event]
-		puts "Event added successfully"
-	else
-		eventsArray=hash[date]
-		eventsArray.each do |i|
-
-			if i.title==title
-				puts "Event already exist with this title"
-				exist=1
-			end			
-		end
-		if exist==0
-			eventsArray.push(event)
-			hash[date]=eventsArray
-			puts "Event added successfully"
-		end
-	end
-end
-
-
-def updateEvent(hash)
-	puts "Update an event"
-
-	begin
-		puts "Enter date in (yyyy-mm-dd) format that you want to update"
-		date=Date.parse(gets.chomp)
-		puts "Enter title that you want to update"
-		name=gets.chomp
-	rescue
-		puts "Please enter valid date"
-		retry
-	end
-	if hash.key?(date)
-		eventsArray=hash[date]
-		eventsArray.each do |i|
-		if i.title==name
-			begin
-  			puts "Enter new date in (yyyy-mm-dd) format"
-				newdate=Date.parse(gets.chomp)
-				puts "Enter new title"
-				newtitle=gets.chomp
-				puts "Enter new description"
-				newdescription=gets.chomp
-			rescue
-				puts "Please enter valid date"
-  			retry
-			end
-			eventsArray.delete(i)
-			event=Event.new(newtitle,newdescription,newdate)
-
-			if not hash.key?(newdate)
-				hash[newdate]=[event]
-			else
-				newArray=hash[newdate]
-				newArray.push(event)
-				hash[newdate]=newArray
-			end
-			puts "Event updated successfully"		
-		end
-	end
-	else
-		puts "This event does not exist"
-	end
-end
-
-def deleteEvent(hash)
-	puts "Delete an event"
-	begin
-		puts "Enter date in (yyyy-mm-dd) format"
-		date=Date.parse(gets.chomp)
-		puts "Enter title"
-		name=gets.chomp
-		
-	rescue
-		puts "Please enter valid date"
-		retry
-	end
-	if not hash.key?(date)
-		puts "No Event is registered on this date"
-	else
-		eventsArray=hash[date]
-		eventsArray.each do |i|
-			if i.title==name
-				eventsArray.delete(i)
-				puts "Event Removed successfully"		
-			end
-		end
-	end
-end
-
-
-def monthView(hash)
-	puts "Print month view"
-	begin
-		puts "Enter date in (yyyy-mm-dd) format"
-		date=Date.parse(gets.chomp)
-	rescue
-		puts "Please enter valid date"
-		retry
-	end
-	puts "\nMonth View ".rjust(50) + date.to_s
-	for i in 1..31
-		monthDates=date.year.to_s.chomp+"-"+date.month.to_s.chomp+"-"+i.to_s.chomp
-		if hash.key?(Date.parse(monthDates))
-			print i.to_s.ljust(3) , hash[Date.parse(monthDates)].length() ,"     "
-		else
-			print i.to_s.ljust(3) , "0     "
-		end
-		if i%7==0 or i==31
-			puts "\n\n"
-		end
-	end
-end
-
-def specificDateEvents(hash)
-	puts "Details of events in specific date"
-	begin
-		puts "Enter date in (yyyy-mm-dd) format"
-		date=Date.parse(gets.chomp)
-	rescue
-		puts "Please enter valid date"
-		retry
-	end
-	puts "\nDetail of Events ".rjust(50) + date.to_s + "\n\n"
-	if hash.key?(date)
-		eventsArray=hash[date]
-		eventsArray.each do |i|
-			print i.to_s
-		end
-	
-	else
-		puts "No event is registered on this date"
-	end
+  attr_accessor :title, :description,:date
 
 end
 
+class EventOperations
+  
+  def display_menu
+    puts "Press 1 to add event"
+    puts "Press 2 to update event"
+    puts "Press 3 to delete event"
+    puts "Press 4 to print the month view"
+    puts "Press 5 to print details of events in specific date"
+    puts "Press 6 to print the details of all events of specific month"
+    puts "Press 7 to exit"
+  end
 
-def monthEventDetails(hash)
-	puts "Print Details of events in month"
-	begin
-		puts "Enter date in (yyyy-mm-dd) format"
-		date=Date.parse(gets.chomp)
-	rescue
-		puts "Please enter valid date"
-		retry
-	end
-	puts "\nDetails of events in month ".rjust(50) + date.to_s+"\n\n"
-	for i in 1..31
-		monthDates=date.year.to_s.chomp+"-"+date.month.to_s.chomp+"-"+i.to_s.chomp
-		if hash.key?(Date.parse(monthDates))
-			if hash[Date.parse(monthDates)].length()>1
-				eventsArray=hash[Date.parse(monthDates)]
-				eventsArray.each do |j|
-					puts i.to_s.ljust(3)
-					puts j.to_s
-				end
-			else
-				puts hash[Date.parse(monthDates)][0].to_s
-			end
-		end
-	end
+  def take_input()
+    begin
+      puts "Enter date in (yyyy-mm-dd) format"
+      date=Date.parse(gets.chomp)
+      puts "Enter Title of your event"
+      title=gets.chomp
+      puts "Enter Description of your event"
+      description=gets.chomp
+    rescue
+      puts "Please enter valid date"
+      retry
+    end
+    return Event.new(title, description, date)
+  end
+
+  def add_event(hash, event)
+    puts "Add an event"
+    if not hash.key?(event.date)
+      hash[event.date]=[event]
+      puts "Event added successfully"
+      return true
+    else
+      events_array=hash[event.date]
+      events_array.each do |events|
+        if events.title==event.title
+          puts "Event already exist with this title"
+          return false
+        end     
+      end
+      events_array.push(event)
+      hash[event.date]=events_array
+      puts "Event added successfully"
+      return true
+    end
+  end
+
+  def input_for_event_to_update()
+    begin
+      puts "Enter date in (yyyy-mm-dd) format that you want to update"
+      date=Date.parse(gets.chomp)
+      puts "Enter title that you want to update"
+      name=gets.chomp
+    rescue
+      puts "Please enter valid date"
+      retry
+    end
+    return Event.new(name, "", date)
+  end
+  
+  def input_for_new_event()
+    begin
+      puts "Enter new date in (yyyy-mm-dd) format"
+      newdate=Date.parse(gets.chomp)
+      puts "Enter new title"
+      newtitle=gets.chomp
+      puts "Enter new description"
+      newdescription=gets.chomp
+    rescue
+      puts "Please enter valid date"
+      retry
+    end
+    return Event.new(newtitle, newdescription , newdate)
+  end
+  
+  def update_event(hash,old_event,new_event)
+    puts "Update an event"
+    if hash.key?(old_event.date)
+      events_array=hash[old_event.date]
+      events_array.each do |event|
+        if event.title==old_event.title
+          events_array.delete(event)
+          event=new_event
+          if not hash.key?(new_event.date)
+            hash[new_event.date]=[new_event]
+          else
+            newArray=hash[new_event.date]
+            newArray.push(new_event)
+            hash[new_event.date]=newArray
+          end
+          puts "Event updated successfully"
+          return true
+        end
+      end
+    else
+      puts "This event does not exist"
+      return false
+    end
+  end
+
+  def input_to_delete_event()
+    begin
+      puts "Enter date in (yyyy-mm-dd) format"
+      date=Date.parse(gets.chomp)
+      puts "Enter title that you want to delete"
+      name=gets.chomp
+    rescue
+      puts "Please enter valid date"
+      retry
+    end
+    return Event.new(name, "", date)
+  end
+  
+  def delete_event(hash, event)
+    puts "Delete an event"
+    if not hash.key?(event.date)
+      puts "No Event is registered on this date"
+      return false
+    else
+      events_array=hash[event.date]
+      events_array.each do |events|
+        if events.title==event.title
+          events_array.delete(event)
+          puts "Event Removed successfully"
+          return true   
+        end
+      end
+    end
+  end
+
+  def month_view(hash)
+    puts "Print month view"
+    begin
+      puts "Enter date in (yyyy-mm-dd) format"
+      date=Date.parse(gets.chomp)
+    rescue
+      puts "Please enter valid date"
+      retry
+    end
+    puts "\nMonth View ".rjust(50) + date.to_s
+    for days in 1..Date.new(date.year, date.month, -1).day
+      monthDates= date.year.to_s.chomp+ "-"+ date.month.to_s.chomp+ "-"+ days.to_s.chomp
+      if hash.key?(Date.parse(monthDates))
+        print days.to_s.ljust(3) , hash[Date.parse(monthDates)].length() ,"     "
+      else
+        print days.to_s.ljust(3) , "0     "
+      end
+      if days%7==0 or days==Date.new(date.year, date.month, -1).day
+        puts "\n\n"
+      end
+    end
+  end
+
+  def specific_date_events(hash)
+    puts "Details of events in specific date"
+    begin
+      puts "Enter date in (yyyy-mm-dd) format"
+      date=Date.parse(gets.chomp)
+    rescue
+      puts "Please enter valid date"
+      retry
+    end
+    puts "\nDetail of Events ".rjust(50) + date.to_s + "\n\n"
+    if hash.key?(date)
+      events_array=hash[date]
+      events_array.each do |event|
+        print event.to_s
+      end
+    else
+      puts "No event is registered on this date"
+      return false
+    end
+  end
+
+  def month_event_details(hash)
+    puts "Print Details of events in month"
+    begin
+      puts "Enter date in (yyyy-mm-dd) format"
+      date=Date.parse(gets.chomp)
+    rescue
+      puts "Please enter valid date"
+      retry
+    end
+    puts "\nDetails of events in month ".rjust(50) + date.to_s+"\n\n"
+    for days in 1..Date.new(date.year, date.month, -1).day
+      monthDates=date.year.to_s.chomp+ "-"+ date.month.to_s.chomp+ "-"+ days.to_s.chomp
+      if hash.key?(Date.parse(monthDates))
+        if hash[Date.parse(monthDates)].length()>1
+          events_array=hash[Date.parse(monthDates)]
+          events_array.each do |event|
+            puts event.to_s
+          end
+        else
+          puts hash[Date.parse(monthDates)].first.to_s
+        end
+      end
+    end
+  end
+
 end
 
+class Driver
+  def run
+    event_operations=EventOperations.new
+    hash=Hash.new
+    puts "****** Welcome to calender *******"
+    event_operations.display_menu
+    while a=gets
+      a=a.chomp
+      if a=="7"
+        puts "Calender is closed successfully"
+        return
+      elsif a=="1"
+        event=event_operations.take_input
+        event_operations.add_event(hash,event)  
+      elsif a=="2"
+        old_event= event_operations.input_for_event_to_update()
+        new_event= event_operations.input_for_new_event()
+        event_operations.update_event(hash, old_event, new_event) 
+      elsif a=="3"
+        event=event_operations.input_to_delete_event
+        event_operations.delete_event(hash,event)
+      elsif a=="4"
+        event_operations.month_view(hash)
+      elsif a=="5"
+        event_operations.specific_date_events(hash)
+      elsif a=="6"
+        event_operations.month_event_details(hash) 
+      elsif a=="8"
+        hash.each do |key,value|
+          puts value
+        end
+      else
+        puts "Please select valid option"
+      end
 
-menu=Menu.new
-hash=Hash.new
-puts "****** Welcome to calender *******"
-menu.displayMenu
-while a=gets.chomp
-	if a=="7"
-		puts "Calender is closed successfully"
-		return
-	elsif a=="1"
-		addEvent(hash)	
-	elsif a=="2"
-		updateEvent(hash)	
-	elsif a=="3"
-		deleteEvent(hash)
-	elsif a=="4"
-		monthView(hash)
-	elsif a=="5"
-		specificDateEvents(hash)
-	elsif a=="6"
-		monthEventDetails(hash)	
-	elsif a=="8"
-		hash.each do |key,value|
-			puts value
-		end
-	end
-	menu.displayMenu
-
+      event_operations.display_menu
+    end
+  end
 end
+
+Driver.new.run
---
+++ testing.rb
@@ -0,0 +1,91 @@
+require_relative "assignment.rb"
+
+describe "Calender", "adding events" do
+  
+  it "should add event in a date" do
+    check=EventOperations.new.add_event(Hash.new, Event.new("event1", "description1", "2012-12-12"))==true
+    expect(check).to eql true
+  end
+  
+  it "should not add event with same title" do
+    event_operations=EventOperations.new
+    hash=Hash.new
+    check1=event_operations.add_event(hash, Event.new("event1", "description1", "2012-12-12"))
+    check2=event_operations.add_event(hash, Event.new("event1", "description1", "2012-12-12"))
+    check= check1 && check2
+    expect(check).to eql false
+  end
+
+  it "should add two events in same date" do
+    event_operations=EventOperations.new
+    hash=Hash.new
+    check1=event_operations.add_event(hash, Event.new("event1", "description1", "2012-12-12"))
+    check2=event_operations.add_event(hash, Event.new("event2", "description2", "2012-12-12"))
+    check=check1 && check2
+    expect(check).to eql true
+  end
+
+end
+
+describe "Calender", "update events" do
+  
+  it "should update event if a single event is registered on a day" do
+    event_operations=EventOperations.new
+    hash=Hash.new
+    check1=event_operations.add_event(hash, Event.new("event1", "description1", "2012-12-12"))
+    check2=event_operations.update_event(hash, Event.new("event1", "description1", "2012-12-12"),Event.new("event2", "description2", "2012-12-12"))
+    check=check1 && check2
+    expect(check).to eql true
+  end
+  
+  it "should update event if two events are on same date" do
+    event_operations=EventOperations.new
+    hash=Hash.new
+    check1=event_operations.add_event(hash, Event.new("event1", "description1", "2012-12-12"))
+    check2=event_operations.add_event(hash, Event.new("event2", "description2", "2012-12-12"))
+    
+    check3=event_operations.update_event(hash, Event.new("event1", "description1", "2012-12-12"),Event.new("event3", "description3", "2012-12-12"))
+    check=check1 && check2 && check3
+    expect(check).to eql true
+  end
+
+  it "should not update event if the event is not registered" do
+    event_operations=EventOperations.new
+    hash=Hash.new
+    check= event_operations.update_event(hash, Event.new("event1", "description1", "2012-12-12"),Event.new("event3", "description3", "2012-12-12"))
+    expect(check).to eql false
+  end
+
+end
+
+describe "Calender", "delete events" do
+  
+  it "should delete event if a single event is registered on a day" do
+    event_operations=EventOperations.new
+    hash=Hash.new
+    check1=event_operations.add_event(hash, Event.new("event1", "description1", "2012-12-12"))
+    check2=event_operations.delete_event(hash, Event.new("event1", "description1", "2012-12-12"))
+    check=check1 && check2
+    expect(check).to eql true
+  end
+  
+  it "should delete event if two events are on same date" do
+    event_operations=EventOperations.new
+    hash=Hash.new
+    check1=event_operations.add_event(hash, Event.new("event1", "description1", "2012-12-12"))
+    check2=event_operations.add_event(hash, Event.new("event2", "description2", "2012-12-12"))
+    
+    check3=event_operations.delete_event(hash, Event.new("event2", "description2", "2012-12-12"))
+    check=check1 && check2 && check3
+    expect(check).to eql true
+  end
+
+  it "should not delete event if the event is not registered" do
+    event_operations=EventOperations.new
+    hash=Hash.new
+    check=event_operations.delete_event(hash, Event.new("event3", "description3", "2012-12-12"))
+    expect(check).to eql false
+  end
+
+end
+
